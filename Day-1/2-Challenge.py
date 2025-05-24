dfdd581281810d5997136c74115371e7770c21eb
samishken <samishken@gmail.com>
2025-05-23 21:46:53 -0400

    day one codes

---
+++ Day-1/2-Challenge.py
@@ -0,0 +1,25 @@
+# String Manipulation and Code Intelligence
+
+#print(input("what is your team? ") + " is my team\nChampions!!")
+#print("Hello " + input("What is your name? "))
+# name = input("What is your name? ")
+# print(name)
+
+# name = input("What is your name? ")
+# length = print("Hello " + name + "!")
+# print(len(input("What is your name? ")))
+
+username = input("Enter your username: ")
+length = len(username)
+print(length)
+
+
+glass1 = "milk"
+glass2 = "juice"
+
+glass1 = glass2
+glass2 = glass1
+print(glass1)
+print(glass2)
+
+
